9432768d37420178269d992ec45b8db6d4243244
sb <sb@Shanes-Air.attlocal.net>
2017-04-14 06:47:30 -0700

    No Longer Dependent on ZeptoJS

--- src/core/server/assets/js/responsiveNavbarText.js
+++ src/core/server/assets/js/responsiveNavbarText.js
@@ -1,4 +1,2 @@
-$(function() {
-    var txt = $(window).width() > 700 ? "Search using a card's name, number, set" : "Enter name/number/set";
-    $('.box').attr('placeholder', txt);
-});
+var txt = window.innerWidth > 700 ? "Search using a card's name, number, set" : "Enter name/number/set";
+document.getElementById('searchbar').setAttribute("placeholder",txt)
