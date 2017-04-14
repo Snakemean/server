826100e2fea1c1e291bd0f2bcb540772d317bee5
sb <sb@Shanes-Air.attlocal.net>
2017-04-14 06:28:04 -0700

    Safari bug fixed, truncated content message

--- src/core/server/assets/js/responsiveNavbarText.js
+++ src/core/server/assets/js/responsiveNavbarText.js
@@ -1,4 +1,4 @@
 $(function() {
-    var txt = $(window).width() > 700 ? "Enter a card's name, number, or set." : "Enter card's name, number, or set";
+    var txt = $(window).width() > 700 ? "Enter a card's name, number, or set." : "Enter name/number/set";
     $('.box').attr('placeholder', txt);
 });
